0f2c9c7f26054fbb33610705c1c59569398f1c75
Shanmugapriya <36492718+Supriyawinisri@users.noreply.github.com>
2019-12-03 20:35:55 +0530

    sample maven project for jenkins

---
+++ src/main/java/com/example/jenkins/jenkins.java
@@ -0,0 +1,10 @@
+package com.example.jenkins;
+
+public class jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample execution for Jenkins.....");
+	}
+
+}
